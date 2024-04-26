4b8a96614a628eaff0e91d82ecb59daebac18293
NozoDev <kevinroj326@gmail.com>
2024-04-26 07:08:44 -0500

    custom hooks para hacer el llamdo a la api

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+
+import useFetch from "../hooks/UseFetch";
+const API_KEY = "Vq-91FazZn9Q4dhAJQL1MOWQ0al0C8-PmTrp5-b4UdE";
+const IMAGE_COUNT = 40;
+
+const Gallery = () => {
+  const { data, hasError, isLoading } = useFetch(
+    `https://api.unsplash.com/photos?client_id=${API_KEY}&per_page=40`
+  );
+
+  const bio = data?.user?.bio;
+  return (
+    <div>
+      <h1>Galeria</h1>
+
+      <div>
+        {data?.map((image) => (
+          <img
+            key={image.id}
+            src={image.urls.regular}
+            alt={image.alt_description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
